a4e50be73374c18a95dcb8f07f50301ac55fef76
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-12-19 18:49:27 +0200

    chore: Initialize project

--- components/Numbers.jsx
+++ components/Numbers.jsx
@@ -5,13 +5,13 @@ function Numbers() {
     <section className="text-[#072140] px-[24px] ">
       <h2 className="text-[32px] ">TUM in numbers</h2>
 
-<div className="gap-[1.5rem] grid grid-rows-3 mt-[2.5rem] lg:grid-cols-3 ">
-<div className="text-white bg-[#3070B3] lg:row-span-1 p-[20px] text-center ">
+<div className="gap-[1.5rem] grid grid-rows-3 mt-[2.5rem]  ">
+<div className="text-white bg-[#3070B3]  p-[20px] text-center ">
         <div className="text-[80px] font-bold">50,000</div>
         <p className="text-[20px] font-bold mt-[8px]">Student</p>
       </div>
 
-<div className="lg:grid grid-cols gap-10 lg:grid-rows-2">
+<div className="lg:grid gap-10 lg:grid-cols-2">
 <div className="text-[#072140] bg-[#f7b11e] p-[20px] text-center ">
         <div className="text-[80px] font-bold">176</div>
         <p className="text-[20px] font-bold mt-[8px]">ERC Grants for cutting-edge research</p>
